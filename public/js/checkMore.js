3da85ad421dbdb403f241a25548c4a1cb33c5d73
mukuashi [穆跨世] <mukuashi@xiaomi.com>
2016-12-08 20:32:06 +0800

    adjust style

--- public/js/checkMore.js
+++ public/js/checkMore.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 	//copy url
 	$(".api-url-copy").on("click",function(){
+		//获得需要复制的内容  input框获取值用val()
+		var apiUrlCopyContent = $(".apiUrlCopy").attr('href');
+			alert(apiUrlCopyContent)
+
 		layer.msg('好的，链接已经复制到剪贴板', {icon: 6});
 	});
-
+	//delete json
 	$(".api-delete").on("click",function(){
 		layer.confirm('你确定要删除该条数据？', {
 		  btn: ['确定','取消'] //按钮
